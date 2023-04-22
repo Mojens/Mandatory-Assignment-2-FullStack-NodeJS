4d0265511ef680939dade65c312b5b0ca710009a
mojens <moha135h@stud.kea.dk>
2023-04-23 01:46:51 +0200

    fix toastr notification

--- client/src/pages/login/Login.svelte
+++ client/src/pages/login/Login.svelte
@@ -1,1 +1,29 @@
-<h1>This is a login page</h1>
+<script>
+    import toastr from "toastr";
+
+    let email = "test@outlook.dk";
+    let password = "1234";
+
+    function handleLogin() {
+        toastr.success("Login successful!");
+        //toastr.error("Login failed!");
+    }
+</script>
+
+<main>
+    <h1>Login</h1>
+
+    <form on:submit|preventDefault={handleLogin}>
+        <label>
+            Email:
+            <input type="email" bind:value={email} required />
+        </label>
+
+        <label>
+            Password:
+            <input type="password" bind:value={password} required />
+        </label>
+
+        <button type="submit">Log in</button>
+    </form>
+</main>
