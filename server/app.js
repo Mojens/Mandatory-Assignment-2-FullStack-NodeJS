f278d60ba4a2af9974b891807747351361ec7a05
mojens <moha135h@stud.kea.dk>
2023-04-23 11:14:59 +0200

    setup session and rate-limit

--- server/app.js
+++ server/app.js
@@ -9,6 +9,24 @@ app.use(cors({
     origin: true,
 }));
 
+import session from 'express-session';
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+import rateLimit from 'express-rate-limit';
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(apiLimiter);
+
+
 
 
 import loginRouter from './routers/loginRouter.js';
